9b00e536143aebdd809a007c53508db6de63dccb
Divyansh-lab123 <2021wb86747@wilp.bits-pilani.ac.in>
2025-06-15 18:23:28 +0530

    Update HelloWorld.java

--- src/main/java/com/sacoit/HelloWorld.java
+++ src/main/java/com/sacoit/HelloWorld.java
@@ -2,10 +2,7 @@ package com.sacoit;
 
 public class HelloWorld {
     public static void main(String[] args) {
-HEAD
         System.out.println("Hello from SonarQube Maven Project!");
-
         System.out.println("Hello, SonarQube Inspection!");
- 59a5dfc1f091f5c60d2ece117a9c0de81d9f1f74
     }
 }
